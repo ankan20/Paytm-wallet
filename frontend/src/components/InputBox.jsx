85dbd01ba6c9abb691d8e6d780aa94a912e191a6
Ankan Das <127923458+ankan20@users.noreply.github.com>
2024-06-16 22:26:24 +0530

    added frontend small components

--- frontend/src/components/InputBox.jsx
+++ frontend/src/components/InputBox.jsx
@@ -1,14 +1,9 @@
-import React from 'react'
 
-const InputBox = ({label,placeholder}) => {
-  return (
-    <div>
-      <div className='text-sm font-medium text-left py-2'>
+export function InputBox({label, placeholder, onChange}) {
+    return <div>
+      <div className="text-sm font-medium text-left py-2">
         {label}
       </div>
-      <input type="text"  placeholder={placeholder} className='w-full  px- py-1 border'/>
+      <input onChange={onChange} placeholder={placeholder} className="w-full px-2 py-1 border rounded border-slate-200" />
     </div>
-  )
 }
-
-export default InputBox
