8cf706f538602f5b5126c8d695c33687127cd006
Ankan Das <127923458+ankan20@users.noreply.github.com>
2024-07-01 12:27:13 +0530

    port added

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app =express();
 const mainRouter = require("./routes/index");
 const cors = require('cors');
+require('dotenv').config()
 
 app.use(cors());
 app.use(express.json());
@@ -9,6 +10,6 @@ app.use(express.json());
 
 app.use("/api/v1",mainRouter);
 
-app.listen(3000,()=>{
+app.listen(process.env.PORT,()=>{
     console.log("Server running on port 3000")
 })
